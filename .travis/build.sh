f25b85dddd543d9ecfa95ccdae45c5651354b062
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2018-04-09 13:17:33 +0200

    Validate desktop and appdata in CI

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,6 +4,12 @@ set -e
 
 source /usr/local/share/liri-travis/functions
 
+# Install packages
+travis_start "install_packages"
+msg "Install packages..."
+sudo apt-get install -y desktop-file-utils appstream-util
+travis_end "install_packages"
+
 # Install artifacts
 travis_start "artifacts"
 msg "Install artifacts..."
@@ -31,3 +37,11 @@ qbs -d build -j $(nproc) --all-products profile:travis-qt5 \
     modules.lirideployment.qmlDir:/usr/lib/x86_64-linux-gnu/qt5/qml \
     modules.lirideployment.pluginsDir:/usr/lib/x86_64-linux-gnu/qt5/plugins
 travis_end "build"
+
+# Validate desktop file and appdata
+for filename in $(find . -type f -name "*.desktop"); do
+    desktop-file-validate $filename
+done
+for filename in $(find . -type f -name "*.appdata.xml"); do
+    appstream-util validate-relax --nonet $filename
+done
