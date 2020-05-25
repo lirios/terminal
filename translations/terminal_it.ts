69c6a98d192340bb857a6c42fd2d5005d705df9f
Liri CI <ci@liri.io>
2020-05-25 00:36:35 +0000

    Automatic merge of Transifex translations
    [ci skip]

---
+++ translations/terminal_it.ts
@@ -0,0 +1,187 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="it" version="2.1">
+<context>
+    <name>AddPasswordDialog</name>
+    <message>
+        <location filename="../src/app/qml/AddPasswordDialog.qml" line="28"/>
+        <source>Add Password</source>
+        <translation>Aggiungi password</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AddPasswordDialog.qml" line="54"/>
+        <source>Title</source>
+        <translation>Titolo</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AddPasswordDialog.qml" line="63"/>
+        <source>Password</source>
+        <translation>Password</translation>
+    </message>
+</context>
+<context>
+    <name>ConfirmCloseDialog</name>
+    <message>
+        <location filename="../src/app/qml/ConfirmCloseDialog.qml" line="35"/>
+        <source>Close tab?</source>
+        <translation>Chiudi scheda?</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/ConfirmCloseDialog.qml" line="35"/>
+        <source>Close Terminal?</source>
+        <translation>Chiudi terminale?</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/ConfirmCloseDialog.qml" line="37"/>
+        <source>There is a process running in this tab. If you close the tab, the process will end.</source>
+        <translation>C&apos;è un processo in esecuzione in questa scheda. Se chiudi la scheda il processo terminerà.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/ConfirmCloseDialog.qml" line="39"/>
+        <source>There are %1 processes running in this window. If you quit Terminal, the processes will end.</source>
+        <translation>Ci sono %1 processi in esecuzione in questa finestra. Se chiudi il terminale i processi termineranno.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/ConfirmCloseDialog.qml" line="40"/>
+        <source>There is a process running in this window. If you quit Terminal, the process will end.</source>
+        <translation>C&apos;è un processo in esecuzione in questa finestra. Se chiudi il terminale il processo terminerà.</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordsDialog</name>
+    <message>
+        <location filename="../src/app/qml/PasswordsDialog.qml" line="28"/>
+        <source>Passwords</source>
+        <translation>Password</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/PasswordsDialog.qml" line="45"/>
+        <source>Add your passwords here for easy access when using sudo, SSH, or other commands that require passwords.</source>
+        <translation>Aggiungi le tue password qui per un facile accesso quando si usa sudo, SSH o altri comandi che richiedono password.</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/PasswordsDialog.qml" line="52"/>
+        <source>Add Password</source>
+        <translation>Aggiungi password</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/PasswordsDialog.qml" line="64"/>
+        <source>n.a.</source>
+        <translation>n.d.</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../src/app/qml/SettingsDialog.qml" line="32"/>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/SettingsDialog.qml" line="50"/>
+        <source>Background opacity</source>
+        <translation>Opacità dello sfondo</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/SettingsDialog.qml" line="70"/>
+        <source>Font size</source>
+        <translation>Dimensione del font</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/SettingsDialog.qml" line="90"/>
+        <source>Font family</source>
+        <translation>Famiglia del font</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/SettingsDialog.qml" line="105"/>
+        <source>Color scheme</source>
+        <translation>Schema di colori</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/SettingsDialog.qml" line="136"/>
+        <source>Smart copy/paste</source>
+        <translation>Copia/Incolla intelligente</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/SettingsDialog.qml" line="158"/>
+        <source>Shell program</source>
+        <translation>Programma shell</translation>
+    </message>
+</context>
+<context>
+    <name>SudoWarningDialog</name>
+    <message>
+        <location filename="../src/app/qml/SudoWarningDialog.qml" line="33"/>
+        <source>Paste this command?</source>
+        <translation>Incolla questo comando?</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/SudoWarningDialog.qml" line="34"/>
+        <source>This command is asking for administrative access to your computer.
+Copying commands from the internet can be dangerous.
+Be sure you understand what this command does before running it:</source>
+        <translation>Questo comando richiede accesso amministrativo al tuo computer.
+Copiare comandi da Internet può essere pericoloso.
+Sii certo di comprendere cosa fa questo comando prima di eseguirlo:</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/SudoWarningDialog.qml" line="39"/>
+        <source>Paste Anyway</source>
+        <translation>Incolla comunque</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/SudoWarningDialog.qml" line="47"/>
+        <source>Don&apos;t Paste</source>
+        <translation>Non incollare</translation>
+    </message>
+</context>
+<context>
+    <name>TerminalTab</name>
+    <message>
+        <location filename="../src/app/qml/TerminalTab.qml" line="41"/>
+        <source>Terminal %1</source>
+        <translation>Terminale %1</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../src/app/qml/main.qml" line="169"/>
+        <source>Copy</source>
+        <translation>Copia</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/main.qml" line="176"/>
+        <source>Paste</source>
+        <translation>Incolla</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/main.qml" line="183"/>
+        <source>Open new tab</source>
+        <translation>Apri nuova scheda</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/main.qml" line="189"/>
+        <source>Open new window</source>
+        <translation>Apri nuova finestra</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/main.qml" line="211"/>
+        <source>Rename...</source>
+        <translation>Rinomina...</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/main.qml" line="217"/>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/main.qml" line="281"/>
+        <source>Rename</source>
+        <translation>Rinomina</translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/main.qml" line="282"/>
+        <source>Type the new title for the current tab.</source>
+        <translation>Digita il nuovo titolo della scheda corrente.</translation>
+    </message>
+</context>
+</TS>
