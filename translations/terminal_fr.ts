283112893cab877756bd409d0c11000b2b1e7b90
Liri CI <ci@liri.io>
2021-12-08 00:44:51 +0000

    Automatic merge of Transifex translations
    [ci skip]

--- translations/terminal_fr.ts
+++ translations/terminal_fr.ts
@@ -1,4 +1,4 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="fr">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fr" version="2.1">
 <context>
     <name>AddPasswordDialog</name>
     <message>
--- translations/terminal_pl.ts
+++ translations/terminal_pl.ts
@@ -1,4 +1,4 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="pl">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="pl" version="2.1">
 <context>
     <name>AddPasswordDialog</name>
     <message>
